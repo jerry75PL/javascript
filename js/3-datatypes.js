c7f29646e0848e4c117532372664bdd8a92d946d
jteklak <jteklak@gmail.com>
2017-06-10 10:53:43 +0200

    typy danych

--- js/3-datatypes.js
+++ js/3-datatypes.js
@@ -4,6 +4,70 @@
 typ liczbowy
 */
 
-var wyplata=5000;
-var premia=2500;
+var wyplata = 5000;
+var premia = 2500;
 var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+/*
+typ łancuch znaków - string
+*/
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+calkowitaWyplataStr = wyplataStr + "\n" + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+calkowitaWyplataStr = wyplataStr + " \"dodajemy\" " + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+calkowitaWyplataStr = wyplataStr + ' "dodajemy" ' + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+calkowitaWyplataStr = wyplataStr + " 'dodajemy' " + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+
+/*
+metody */
+
+var imieStudenta = " Michał";
+console.log(imieStudenta.indexOf('c'));
+console.log(imieStudenta);
+console.log(imieStudenta.trim());
+
+
+/*
+tyo logiczny boolean*/
+
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog) {
+    console.log("Jest SMOG");
+} else {
+    console.log("Nie ma SMOGa :)");
+}
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog) {
+    console.log("Jest SMOG");
+} else {
+    console.log("Nie ma SMOGa :)");
+}
+
