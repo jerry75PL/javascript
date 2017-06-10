069e215b7c00b298b86f1c987b99696daf25992d
jteklak <jteklak@gmail.com>
2017-06-10 11:55:16 +0200

    obiekty matematyczne

---
+++ js/6-object-math.js
@@ -0,0 +1,13 @@
+'use strict';
+/*obiekt math*/
+
+console.log( Math.PI );
+console.log( Math.cos(0) );
+console.log( Math.E );
+console.log( Math.pow(7, 3) );
+
+//Pole koła Pi*r2 obwód 2*Pi*r
+var r = 10;
+console.log( Math.PI * Math.pow(r, 2));
+console.log( 2 * Math.PI * r);
+alert( "LN10 = " + Math.LN10 );
