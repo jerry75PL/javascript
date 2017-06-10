66da5a0d7c2b921bae095f96f487d8ee6b9d1d1f
jteklak <jteklak@gmail.com>
2017-06-10 14:35:02 +0200

    operatory

---
+++ js/9-operators.js
@@ -0,0 +1,48 @@
+'use strict';
+/*operatory*/
+
+var liczba1 = 13,
+    liczba2 = 5;
+var wynik;
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+console.log('przerwa');
+
+var licz = 2;
+licz += 3;
+//licz= licz +3;
+
+console.log(licz);
+
+var liczba1 = '13',
+    liczba2 = 13;
+if (liczba1 == liczba2) {
+    console.log('równe')
+}
+console.log('przerwa');
+
+if (liczba1 === liczba2) {
+    console.log('równe')
+}
+
+console.log('przerwa3231323');
+
+var liczba1 = 13,
+    liczba2 = 5;
+if (liczba2 > 3 && liczba2 < 10) {
+    console.log('tak');
+}
+
+
+console.log('przefij3');
+
+(2 > 5) ? console.log('TAK'): console.log('nie');
+
+console.log('to samo inaczej zapisane')
+
+if (2 > 5) {
+    console.log('tak');
+} else {
+    console.log('NIE');
+}
